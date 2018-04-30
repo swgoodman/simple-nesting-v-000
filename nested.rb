a9ae88cedc8eb95d31f047923fb7aff851ecac33
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-30 01:05:24 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -113,7 +113,6 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
 alans_new_info = "GUI"
 programmer_hash[:alan_kay][:known_for]
-programmer_hash[:alan_kay][known_for]
 
 end
 
